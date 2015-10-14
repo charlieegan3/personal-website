1221a0e74831765163055ff207d73b92fbf949b0
Charlie Egan <charlieegan3@googlemail.com>
2015-10-14 11:01:47 +0100

    Only fade in later entries

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,21 +1,14 @@
 function displayVisibleEntries() {
   var windowBottom = $(window).scrollTop() + $(window).height();
-  $('.entry').each(function(i){
+  $('.later').each(function(i){
     if(windowBottom > $(this).offset().top){
-      $(this).animate({'opacity':'1', 'margin': '7px 0px'}, 400);
+      $(this).animate({'opacity':'1', 'margin-top': '7px'}, 400);
     }
   });
 }
 
 $(document).ready(function() {
-  var count = 0;
-  while(true) {
-    count++;
-    if ($('.entry').length > 0 || count > 500) {
-      displayVisibleEntries();
-      break;
-    }
-  }
+  displayVisibleEntries();
   $(window).scroll(function(){
     displayVisibleEntries();
   });
