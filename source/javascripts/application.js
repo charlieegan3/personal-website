3568b6320b161d7721948c87b3e67d2ff0fddc01
Charlie Egan <charlieegan3@users.noreply.github.com>
2017-03-19 17:38:47 +0000

    Taken -> posted for IG photo captions

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -122,9 +122,9 @@ function setLiveContent(data) {
   $("#instagram-link").attr("href", data.image.link);
   $("#instagram-image").attr("src", data.image.images.standard_resolution);
   if (data.image.location != null) {
-    $("#instagram-meta").text(data.image.location + ", " + data.image.created_ago);
+    $("#instagram-meta").text(data.image.location + ", posted " + data.image.created_ago);
   } else {
-    $("#instagram-meta").text("taken " + data.image.created_ago);
+    $("#instagram-meta").text("posted " + data.image.created_ago);
   }
 }
 
