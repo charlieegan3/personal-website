e24d786824ece92008fda456bef52cc5b4ee8122
Charlie Egan <charlieegan3@users.noreply.github.com>
2017-04-15 19:04:59 +0100

    Draft home page

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -2,3 +2,15 @@
 
 if (window.location.hostname == "www.charlieegan3.com")
   window.location.hostname = "charlieegan3.com";
+
+var hourOfDay = new Date().getHours();
+var greeting = "Hello";
+
+if (hourOfDay < 12 && hourOfDay > 2) {
+  greeting = "Good morning";
+} else if (hourOfDay < 19 && hourOfDay > 2) {
+  greeting = "Good afternoon";
+} else {
+  greeting = "Good evening";
+}
+document.getElementById("greeting").innerHTML = greeting;
