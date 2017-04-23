a90042172344ebfba095421762177445206100f7
Charlie Egan <charlieegan3@users.noreply.github.com>
2017-04-23 12:48:30 +0100

    Restart timer on visit

--- source/javascripts/pagetimes.js
+++ source/javascripts/pagetimes.js
@@ -1,5 +1,16 @@
-if (document.cookie.indexOf("dnt") === -1) {
-  setInterval(function() {
+function setTimer() {
+  return setInterval(function() {
     logger.send("pagetimes");
   }, 10000);
+}
+
+if (document.cookie.indexOf("dnt") === -1) {
+  var pagetimer = setTimer();
+
+  document.addEventListener("turbolinks:visit", function(event) {
+    if (typeof pagetimer !== "undefined") {
+      clearInterval(pagetimer);
+      pagetimer = setTimer();
+    }
+  });
 }
