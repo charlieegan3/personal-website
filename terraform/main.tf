1eabca45e361ed75c0fe0900b7063aa6f33e58c8
Charlie Egan <charlieegan3@users.noreply.github.com>
2017-07-13 00:16:01 +0100

    Add bucket for generated site

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,10 @@
+variable "project" {
+  default = "charlieegan3-www"
+}
+
+# Infrastructure State
 resource "aws_s3_bucket" "state" {
-  bucket = "charlieegan3-www-state"
+  bucket = "${var.project}-state"
 }
 
 data "terraform_remote_state" "www" {
@@ -9,5 +14,31 @@ data "terraform_remote_state" "www" {
     bucket = "${aws_s3_bucket.state.id}"
     region = "${aws_s3_bucket.state.region}"
     key    = "terraform.tfstate"
+  }
+}
+
+# Website
+resource "aws_s3_bucket" "www" {
+  bucket = "${var.project}-build"
+  acl    = "public-read"
+
+  policy = <<EOF
+{
+    "Statement": [ {
+        "Action": [
+            "s3:GetObject"
+        ],
+        "Effect": "Allow",
+        "Resource": "arn:aws:s3:::${var.project}-build/*",
+        "Principal": {
+            "AWS": [ "*" ]
+        }
+    } ]
+}
+  EOF
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
   }
 }
