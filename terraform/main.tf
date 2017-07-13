e4e3632602fd7d9c5489fe6f0dfc19ddbf2d308d
Charlie Egan <charlieegan3@users.noreply.github.com>
2017-07-13 22:07:52 +0100

    Configure Cloudfront distribution

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,3 +36,55 @@ resource "aws_s3_bucket" "www" {
     error_document = "error.html"
   }
 }
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = "${aws_s3_bucket.www.bucket_domain_name}"
+    origin_id   = "${aws_s3_bucket.www.id}"
+  }
+
+  enabled         = true
+  is_ipv6_enabled = true
+
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "${aws_s3_bucket.www.id}"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    min_ttl     = 0
+    default_ttl = 3600
+    max_ttl     = 86400
+
+    compress = true
+  }
+
+  custom_error_response {
+    error_code         = "404"
+    response_code      = "404"
+    response_page_path = "/error.html"
+  }
+
+  price_class = "PriceClass_200"
+
+  "restrictions" {
+    "geo_restriction" {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
