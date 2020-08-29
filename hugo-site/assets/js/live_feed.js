56a124da21803e15d12d5a70a5ca4ad4381b4e46
Charlie Egan <charlieegan3@users.noreply.github.com>
2020-08-29 16:10:16 +0100

    Fetch json data from gh pages

--- hugo-site/assets/js/live_feed.js
+++ hugo-site/assets/js/live_feed.js
@@ -90,7 +90,7 @@ var liveFeed = {};
       return;
 
     var request = new XMLHttpRequest();
-    request.open("GET", "/data/status.json", true);
+    request.open("GET", "https://charlieegan3.github.io/json-charlieegan3/build/status.json", true);
 
     request.onload = function() {
       if (request.status >= 200 && request.status < 400) {
