12e03021a8511d1fc06c68801874220dc078ce77
Charlie Egan <charlieegan3@users.noreply.github.com>
2020-08-31 22:38:07 +0100

    Allow setting of commit detail

--- bin/commit.rb
+++ bin/commit.rb
@@ -9,6 +9,11 @@ if ARGV.length < 1
   fail "missing version name as first arg"
 end
 
+detail = ""
+if ARGV.length == 2
+  detail = ARGV[1]
+end
+
 # configure git if needed
 email = `git config --global user.email`.chomp
 name = `git config --global user.name`.chomp
@@ -20,5 +25,5 @@ end
 
 # commit the content changes
 run("git add content")
-run("git commit -m '#{ARGV[0]}'")
+run("git commit -m '#{ARGV[0]}' -m '#{detail}'")
 run("git push origin master")
